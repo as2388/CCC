485177f5459c06d6bc9d1cc9c273c845aa210a93
Alexander Simpson <as2388@cam.ac.uk>
2016-04-20 22:02:23 +0100

    init

---
+++ extension.js
@@ -0,0 +1,66 @@
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {
+        socket.close();
+    };
+
+    var socket = null;
+    var pendingMessages = [];
+    var awaitingUsername = true;
+
+    function sendMessage(message) {
+        socket.send(message);
+    }
+
+    ext.connect = function(addr, username, callback) {
+        if (socket != null) {
+            socket.close();
+            awaitingUsername = true;
+        }
+
+        socket = new WebSocket("ws://" + addr + ":1337");
+
+        socket.onopen = function(event) {
+            sendMessage(username);
+        }   
+
+        socket.onmessage = function(event) {
+            var received = event.data;
+            if (awaitingUsername) {
+                awaitingUsername = false;
+                callback();
+            }
+
+            pendingMessages.push(received);
+        }
+    }
+
+    ext.getNextMessage = function() {
+        if (pendingMessages == []) {
+            return "";
+        } else {
+            return pendingMessages.pop();
+        }
+    }
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            ['w', 'connect to %s as user %s', 'connect', '', ''],
+            ['w', 'send message %s', 'sendMessage', 'Hello, World!'],
+            ['r', 'next message', 'getNextMessage']
+        ],
+        menus: {
+           
+        }
+    };
+
+    // Register the extension
+    ScratchExtensions.register('Code Club Chat', descriptor, ext);
+})({});
